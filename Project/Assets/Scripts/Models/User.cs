0635b996054b7dcc50cf826a5e70d7b1a8310d18
Callum Thompson <36245864+Lumbo1379@users.noreply.github.com>
2021-01-26 17:45:08 +0000

    Datbase and authentication init

---
+++ Project/Assets/Scripts/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using Firebase.Firestore;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[FirestoreData]
+public class User
+{
+    [FirestoreProperty]
+    public string Uid { get; set; }
+    public float Score { get; set; }
+}
